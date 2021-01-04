9bd6cc5e0823df4a96dc4bd6adbca1c71b9ad86e
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2021-01-04 11:27:55 +0300

    Init

---
+++ assets/js/mediabox.14a23168.js
@@ -0,0 +1,294 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"../../node_modules/mediabox/dist/mediabox.js":[function(require,module,exports) {
+var define;
+/*! mediabox v1.1.3 | (c) 2018 Pedro Rogerio | https://github.com/pinceladasdaweb/mediabox */
+(function (root, factory) {
+    "use strict";
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.MediaBox = factory();
+    }
+}(this, function () {
+    "use strict";
+
+    var MediaBox = function (element, params) {
+        var default_params = { autoplay: '1' },
+            params = params || 0;
+
+        if (!this || !(this instanceof MediaBox)) {
+            return new MediaBox(element, params);
+        }
+
+        if (!element) {
+            return false;
+        }
+
+        this.params = Object.assign(default_params, params);
+        this.selector = element instanceof NodeList ? element : document.querySelectorAll(element);
+        this.root     = document.querySelector('body');
+        this.run();
+    };
+
+    MediaBox.prototype = {
+        run: function () {
+            Array.prototype.forEach.call(this.selector, function (el) {
+                el.addEventListener('click', function (e) {
+                    e.preventDefault();
+
+                    var link = this.parseUrl(el.getAttribute('href'));
+                    this.render(link);
+                    this.events();
+                }.bind(this), false);
+            }.bind(this));
+
+            this.root.addEventListener('keyup', function (e) {
+                if ((e.keyCode || e.which) === 27) {
+                    this.close(this.root.querySelector('.mediabox-wrap'));
+                }
+            }.bind(this), false);
+        },
+        template: function (s, d) {
+            var p;
+
+            for (p in d) {
+                if (d.hasOwnProperty(p)) {
+                    s = s.replace(new RegExp('{' + p + '}', 'g'), d[p]);
+                }
+            }
+            return s;
+        },
+        parseUrl: function (url) {
+            var service = {},
+                matches;
+
+            if (matches = url.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/)) {
+                service.provider = "youtube";
+                service.id       = matches[2];
+            } else if (matches = url.match(/https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/)) {
+                service.provider = "vimeo";
+                service.id       = matches[3];
+            } else {
+                service.provider = "Unknown";
+                service.id       = '';
+            }
+
+            return service;
+        },
+        render: function (service) {
+            var embedLink,
+                lightbox,
+                urlParams;
+
+            if (service.provider === 'youtube') {
+                embedLink = 'https://www.youtube.com/embed/' + service.id;
+            } else if (service.provider === 'vimeo') {
+                embedLink = 'https://player.vimeo.com/video/' + service.id;
+            } else {
+                throw new Error("Invalid video URL");
+            }
+
+            urlParams = this.serialize(this.params);
+
+            lightbox = this.template(
+                '<div class="mediabox-wrap" role="dialog" aria-hidden="false"><div class="mediabox-content" role="document" tabindex="0"><span id="mediabox-esc" class="mediabox-close" aria-label="close" tabindex="1"></span><iframe src="{embed}{params}" frameborder="0" allowfullscreen></iframe></div></div>', {
+                    embed: embedLink,
+                    params: urlParams
+                });
+
+            this.lastFocusElement = document.activeElement;
+            this.root.insertAdjacentHTML('beforeend', lightbox);
+            document.body.classList.add('stop-scroll');
+        },
+        events: function () {
+            var wrapper = document.querySelector('.mediabox-wrap');
+            var content = document.querySelector('.mediabox-content');
+
+            wrapper.addEventListener('click', function (e) {
+                if (e.target && e.target.nodeName === 'SPAN' && e.target.className === 'mediabox-close' || e.target.nodeName === 'DIV' && e.target.className === 'mediabox-wrap' || (e.target.className === 'mediabox-content' && e.target.nodeName !== 'IFRAME')) {
+                    this.close(wrapper);
+                }
+            }.bind(this), false);
+
+            document.addEventListener('focus', function(e) {
+                if (content && !content.contains(e.target)) {
+                    e.stopPropagation();
+                    content.focus();
+                }
+            }, true);
+
+            content.addEventListener('keypress', function(e) {
+                if (e.keyCode === 13) {
+                    this.close(wrapper);
+                }
+            }.bind(this), false);
+        },
+        close: function (el) {
+            if (el === null) return true;
+            var timer = null;
+
+            if (timer) {
+                clearTimeout(timer);
+            }
+
+            el.classList.add('mediabox-hide');
+
+            timer = setTimeout(function() {
+                var el = document.querySelector('.mediabox-wrap');
+                if (el !== null) {
+                    document.body.classList.remove('stop-scroll');
+                    this.root.removeChild(el);
+                    this.lastFocusElement.focus();
+                }
+            }.bind(this), 500);
+        },
+        serialize: function (obj) {
+            return '?'+Object.keys(obj).reduce(function(a,k){a.push(k+'='+encodeURIComponent(obj[k]));return a},[]).join('&')
+        }
+    };
+
+    return MediaBox;
+}));
+
+/**
+ * Object.assign polyfill for IE support
+ * Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+ */
+if (typeof Object.assign != 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, "assign", {
+        value: function assign(target, varArgs) { // .length of function is 2
+        'use strict';
+        if (target == null) { // TypeError if undefined or null
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) { // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+                }
+            }
+            }
+        }
+        return to;
+        },
+        writable: true,
+        configurable: true
+    });
+}
+},{}]},{},["../../node_modules/mediabox/dist/mediabox.js"], null)
+//# sourceMappingURL=/mediabox.14a23168.map
